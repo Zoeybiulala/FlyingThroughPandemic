28f572b828ee61c4a20a3d47e09e1a2e60540c84
Zoeyabiubiu <1064271634@qq.com>
2023-02-05 20:49:00 -0500

    init

---
+++ project/server/db-config.js
@@ -0,0 +1,8 @@
+// Private. Will not be included in submission
+module.exports = {
+  host: "database-1.cjk1ipa2trrx.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user : "admin",
+  password: "bscsqld23nqzs",
+  database: "FAMILY_AND_FRIENDS"
+};
